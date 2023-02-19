de1ae5bf1eb64f0b8264c3f377b9e8f5de9ae38c
asumanodabasi <asumanodabasi1@gmail.com>
2023-02-19 16:08:01 +0200

    User and register created

---
+++ Business/BusinessAspects/Autofac/SecuredOperation.cs
@@ -0,0 +1,40 @@
+﻿using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Castle.DynamicProxy;
+using Microsoft.Extensions.DependencyInjection;
+using Core.Extensions;
+using Business.Constants;
+
+namespace Business.BusinessAspects.Autofac
+{
+                 // for JWT
+    public class SecuredOperation : MethodInterception
+    {
+        private string[] _roles;
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public SecuredOperation(string roles)
+        {
+            _roles = roles.Split(',');
+            _httpContextAccessor = ServiceTool.ServiceProvider.GetService<IHttpContextAccessor>(); //Autofac te injection degerlerini alir
+
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var roleClaims = _httpContextAccessor.HttpContext.User.ClaimRoles();
+            foreach (var role in _roles)
+            {
+                if (roleClaims.Contains(role)) //claimrollerin icinde role varsa a
+                {
+                    return;        //metotu calismaya devam ettir
+                }
+            }
+            throw new Exception(Messages.AuthorizationDenied);
+        }
+    }
+}
---
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Encryption
+{
+  public  class SigningCredentialsHelper
+    {                        //Credentials: kullanici bilgileri
+        public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey)
+        {                              //AspNet e hangi anahtari ve algoritmayi vericegimizi soyledik
+            return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+        }
+    }
+}
